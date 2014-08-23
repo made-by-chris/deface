538630ac158b37ba82d7661b48e193442c6cd4d3
basiclaser <basiclaser@live.com>
2014-08-23 22:16:23 +0200

    cleaned up redundant files

--- chrome/contenscript.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  // Trollface image must be at 'my_extension/images/trollface.jpg'.
-  var trollface = chrome.extension.getURL("images/trollface.jpg");
-  $('#content article img').each(function(index, image){
-    $(image).attr('src', trollface);
-  });
-});
