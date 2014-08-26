2d3cda5d53c0c5acffcaf9e5a0f07b378d78396c
basiclaser <basiclaser@live.com>
2014-08-26 18:23:33 +0200

    emoji bugfix

--- firefox/data/deface.js
+++ firefox/data/deface.js
@@ -1,7 +1,10 @@
+//splash
+// document.body.insertAdjacentHTML('afterbegin','<div id=\"splash\"></div>');
+
 //inject CSS
 document.body.classList.add("deface");
 //inject title
-setTimeout("document.title=\"deface\"",7000);
+// setInterval("document.title=\"deface\"",5000);
 //inject message
 setTimeout("document.body.insertAdjacentHTML('beforeend','<p style=\"font-size:1rem;position:fixed;left:50px;top:0;\" class=\"selno cli\" >Be careful. All conversations are monitored. <a href=\"https://twitter.com/basiclaser\" target=\"_blank\">deface</a><span class=\"cursor\">|</span></p>');",3000);
 
@@ -13,8 +16,27 @@ document.body.appendChild(html);
 document.getElementById('defaceButton').addEventListener('click', function() {
     if (document.body.classList.contains('deface')) {
         document.body.classList.remove('deface');
-        setTimeout('document.body.classList.add(\'deface\')',30000);
+        // setTimeout('document.body.classList.add(\'deface\');',60000);
     } else {
         document.body.classList.add('deface');
     }
 });
+
+// // FADE OUT THE FLASH SCREEN
+// var splash = document.getElementById("splash");
+//
+// function fadeout(el) {
+//   el.style.opacity = 1;
+//
+//   var tick = function() {
+//     el.style.opacity = el.style.opacity - 0.1
+//     if (+el.style.opacity > 0) {
+//       (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
+//     }
+//     // if(+el.style.opacity == 0){splash.style.display = 'none';}
+//   };
+//
+//   tick();
+// }
+// setTimeout('fadeout(splash);',3000);
+// splash.style.display = 'none'
